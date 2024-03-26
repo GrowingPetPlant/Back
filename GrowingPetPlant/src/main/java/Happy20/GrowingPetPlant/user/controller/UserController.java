7ca9d82b33756dfc620cac4532358bd15931274c
YU JEONG SEON <yjs000616@gmail.com>
2024-03-26 16:53:30 +0900

    [FEAT/#1] 로그인

--- GrowingPetPlant/src/main/java/Happy20/GrowingPetPlant/user/controller/UserController.java
+++ GrowingPetPlant/src/main/java/Happy20/GrowingPetPlant/user/controller/UserController.java
@@ -3,19 +3,26 @@ package Happy20.GrowingPetPlant.user.controller;
 import Happy20.GrowingPetPlant.user.domain.User;
 import Happy20.GrowingPetPlant.user.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
-@AllArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/test")
-    public User doTest() {
-        return (userService.makeTestUser());
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
     }
+
+    @GetMapping("/login")
+    public String login(@RequestParam String id, @RequestParam String password){
+        if(userService.validationLogin(id, password)){
+            return "로그인 성공";
+        }
+        return "로그인 실패";
+    }
+
 }
--- GrowingPetPlant/src/main/java/Happy20/GrowingPetPlant/user/domain/User.java
+++ GrowingPetPlant/src/main/java/Happy20/GrowingPetPlant/user/domain/User.java
@@ -5,20 +5,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//data : getter, setter 포함
 @Data
 @Entity
-@Table(name = "gpp_user")
+@Table(name = "user")
 @NoArgsConstructor
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //db가 자동으로 생성(값을 직접 입력할 필요 X, pk이기 때문에)
+    private Long user_number;
 
-    private String name;
+    private String id;
+    private String password;
+    private String user_name;
+    private String phone_number;
 
-    @Builder
-    public User(String name) {
-        this.name = name;
+    //인수없는 생성자 자동으로 생성됨(NoArgsConstructor)
+    //생성자
+    public User(String id, String password, String userName, String phoneNumber){
+        this.id = id;
+        this.password = password;
+        this.user_name = userName;
+        this.phone_number = phoneNumber;
     }
+
 }
