2175e6703de3f1e6096c348cb64e15a02bda4b27
KSJ0128 <ksj01128@naver.com>
2024-08-12 16:18:11 +0900

    로그인시 유저 번호 리턴 삭제

--- GrowingPetPlant/src/main/java/Happy20/GrowingPetPlant/User/DTO/PostLoginRes.java
+++ GrowingPetPlant/src/main/java/Happy20/GrowingPetPlant/User/DTO/PostLoginRes.java
@@ -8,15 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class PostLoginRes {
-    Long userId;
-
     String message;
 
     TokenDto token;
 
     @Builder
-    public PostLoginRes(Long userId, String message, TokenDto token) {
-        this.userId = userId;
+    public PostLoginRes(String message, TokenDto token) {
         this.message = message;
         this.token = token;
     }
